069f769a321ba47ddd9280856902454e12070477
Carlos Javier <carlosmorenoalcivarz@gmail.com>
2023-07-19 23:18:17 -0600

    Implementacion de los views y urls

--- processos/views.py
+++ processos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    return render(request,'login.html',{})
+
+def olvidarControseña(request):
+    return render(request,'',{})
+
+def menu_inicio(request):
+    return render(request,'',{})
+
